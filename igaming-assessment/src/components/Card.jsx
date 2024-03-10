323fc519d5b90d5e92eb3bf12afa11e043ce3768
gnomemage1990@gmail.com <gnomemage1990@gmail.com>
2024-03-10 15:52:47 +0800

    fix: animation

--- igaming-assessment/src/components/Card.jsx
+++ igaming-assessment/src/components/Card.jsx
@@ -4,18 +4,19 @@ function Card({ title, shortDescription, image_path, slug}) {
 
   return (
     <div className={cardStyles.card}>
-      <div className="flex flex-col">
-        <div className="rounded">
-            <img src={image_path} alt='' className={cardStyles.image} />
-        </div>
-        <div className="font-bold pb-4">
-          {title}
-        </div>
-        <div className="text-ellipsis overflow-hidden">
-          {shortDescription}
+      <div className={cardStyles.animationContainer}>
+        <div className="flex flex-col">
+          <div className="rounded">
+              <img src={image_path} alt='' className={cardStyles.image} />
+          </div>
+          <div className="font-bold pb-4">
+            {title}
+          </div>
+          <div className="text-ellipsis overflow-hidden">
+            {shortDescription}
+          </div>
         </div>
       </div>
-      
     </div>
   );
 }
